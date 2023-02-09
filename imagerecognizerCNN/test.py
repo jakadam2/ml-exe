eda4cc627a6329f6c7bc1f63244f24c36dce3e4b
jakadam <adamwozny7@gmail.com>
2023-02-09 21:46:13 +0100

    nice save

--- imagerecognizerCNN/test.py
+++ imagerecognizerCNN/test.py
@@ -2,5 +2,5 @@ from tensorflow import keras
 from image_loader import load_one_image_tocnn
 model = keras.models.load_model('digit_recognizer')
 for i in range(10):
-    print(i,': ',round(model.predict_on_batch(load_one_image_tocnn('img/' + '0.png'))[0][i]*100,2),'%')
+    print(i,': ',round(model.predict_on_batch(load_one_image_tocnn('img/' + '9.png'))[0][i]*100,2),'%')
 
